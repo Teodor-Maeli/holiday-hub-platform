a584fb7e224ae74bbb366f631bc47f0d51f1bf54
TeodorMaeli <teodor.maeli@brainstarstech.com>
2023-06-11 16:11:49 +0300

    added roles to entities.

--- src/main/java/com/platform/authorizationserver/domain/entity/Client.java
+++ src/main/java/com/platform/authorizationserver/domain/entity/Client.java
@@ -1,32 +1,39 @@
 package com.platform.authorizationserver.domain.entity;
 
+import com.platform.authorizationserver.model.Role;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Set;
 import java.util.UUID;
-import lombok.Getter;
+import java.util.stream.Collectors;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Getter
 @SuperBuilder
 @MappedSuperclass
-public abstract class Client implements Serializable {
+public abstract class Client implements Serializable, UserDetails {
 
     @Id
+    @Column(updatable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    @Column
+    @Column(updatable = false)
     private LocalDate registeredDate;
-    @Column
-    private String authorities;
+    @Column(updatable = false)
+    private String username;
     @Column
     private String password;
-    @Column
-    private String username;
+    @ElementCollection(targetClass = String.class)
+    private Set<Role> Roles;
     @Column
     private boolean isAccountNonExpired;
     @Column
@@ -35,4 +42,41 @@ public abstract class Client implements Serializable {
     private boolean isCredentialsNonExpired;
     @Column
     private boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Roles.stream()
+            .map(authority -> new SimpleGrantedAuthority(authority.name()))
+            .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnabled;
+    }
 }
---
+++ src/main/java/com/platform/authorizationserver/model/Role.java
@@ -0,0 +1,15 @@
+package com.platform.authorizationserver.model;
+
+/**
+ * 27.05.2023.
+ *
+ * <p>User roles used to determine capabilities.</p>
+ * Since 1.0
+ *
+ * <p>Author : Teodor Maeli</p>
+ */
+public enum Role {
+    ADMIN,
+    PERSON,
+    LEGAL_ENTITY
+}
